2840a68ae980e8016ec4900ac718837c8e91c62a
farrel216 <farrel.arp@gmail.com>
2023-07-01 13:58:54 +0700

    clean code

--- app/about/Experience.jsx
+++ app/about/Experience.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
+import exp from '../component/Constant'
 
-const Details = ({position, company, time, address, description}) =>{
+const Details = ({item}) =>{
+    
     return (
-        <li className='my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between'>
+
+        <div className='my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between'>
             <div>
                 <h3 className='capitalize font-bold text-2xl'>
-                    {position}, <span className='font-normal italic text-xl'>@{company}</span>
+                    {item.position}, <span className='font-normal italic text-xl'>@{item.company}</span>
                 </h3>
-                <span className='font-medium text-dark/70'>{time} | {address}</span>
+                <span className='font-medium text-dark/70'>{item.time} | {item.address}</span>
                 <p className='w-full'>
-                    {description}
+                    {item.description}
                 </p>
             </div>
-        </li>
+        </div>
     )
 }
 
@@ -23,11 +26,11 @@ const Experience = () => {
             Experience
         </h2>
         <div className='w-[75%] mx-auto relative'>
-            <ul>
-                <Details position={'Computer Network Laboratory Assistant'} company={'Diponegoro University'} time={`Feb '23 - May '23`} address={'Semarang, Indonesia'} description={`Facilitated lecturer to provide guidance, discussion, and encourage skills to 58 of total computer science once a week for learning concepts, technologies, and principles used in the development and management of computer networks.`}/>
-                <Details position={'Algorithm Analysis and Strategy Laboratory Assistant'} company={'Diponegoro University'} time={`Feb '23 - May '23`} address={'Semarang, Indonesia'} description={`Facilitated lecturer to provide guidance, discussion, and encourage skills to 53 of total computer science students once a week about how to design, analyze, and implement algorithms efficiently and effectively in solving certain problems.`}/>
-                <Details position={'Web Developer Intern'} company={'PT Rekayasa Agromarin Indonesia'} time={`Aug '22 - Des '22`} address={'Jepara, Indonesia'} description={'Developed a software to receive and collect data from sensors installed in the fishing environment in real time. Part of "Magang Bersertifikat Kampus Merdeka Matching Fund KEDAIREKA"'}/>
-            </ul>
+            <div>
+                {exp.map((item, index) => (
+                    <Details key={index} item={item} />
+                ))}
+            </div>
         </div>
 
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,23 +5,23 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
- 
+
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['var(--font-poppins)'],
-        merriweather: ['var(--font-merriweather)'],
-        roboto_mono: ['var(--font-roboto-mono)']
+        sans: ["var(--font-poppins)"],
+        merriweather: ["var(--font-merriweather)"],
+        roboto_mono: ["var(--font-roboto-mono)"],
       },
       colors: {
-        'dark': '#1b1b1b',
-        'light': '#f7f7f7',
-        'primary': '#4f46e5'
+        dark: "#1b1b1b",
+        light: "#f7f7f7",
+        primary: "#4f46e5",
       },
     },
   },
   plugins: [],
-}
+};
